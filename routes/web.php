05c2e6417dbf0a340a663360dddbed94b22fbb1a
Paride Azzari <paride21azzari@gmail.com>
2024-01-02 23:02:07 +0100

    refactored config

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use Azzarip\NotaCMS\Blog;
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('notacms.blog.path'), function () {
-    $posts = Blog::published();
+Route::get('/{blog}', function(string $blog)  {
+    $model = config('notacms.' . $blog . '.model');
+    $posts = call_user_func([$model, 'published']);
 
-    return view('notacms::index', compact('posts'));
-});
+    return view('notacms::' . $blog . '.index', compact('posts'));
+})->whereIn('blog', array_keys(config('notacms')));
 
-Route::get(config('notacms.blog.path').'/{slug}', function ($slug) {
-    $post = Blog::findSlug($slug);
 
-    return view('notacms::show', compact('post'));
-});
+Route::get('/{blog}/{slug}', function(string $blog, string $slug)  {
+    $model = config('notacms.' . $blog . '.model');
+    $post = call_user_func([$model, 'findSlug'], $slug);
+
+    return view('notacms::' . $blog . '.show', compact('post'));
+})->whereIn('blog', array_keys(config('notacms')));
