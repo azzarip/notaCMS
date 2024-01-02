1036360cbf97bfd0aeecfb08b7a8f8160e829069
azzarip <azzarip@users.noreply.github.com>
2024-01-02 22:04:59 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,17 @@
 <?php
 
-use Azzarip\NotaCMS\Blog;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{blog}', function(string $blog)  {
-    $model = config('notacms.' . $blog . '.model');
+Route::get('/{blog}', function (string $blog) {
+    $model = config('notacms.'.$blog.'.model');
     $posts = call_user_func([$model, 'published']);
 
-    return view('notacms::' . $blog . '.index', compact('posts'));
+    return view('notacms::'.$blog.'.index', compact('posts'));
 })->whereIn('blog', array_keys(config('notacms')));
 
-
-Route::get('/{blog}/{slug}', function(string $blog, string $slug)  {
-    $model = config('notacms.' . $blog . '.model');
+Route::get('/{blog}/{slug}', function (string $blog, string $slug) {
+    $model = config('notacms.'.$blog.'.model');
     $post = call_user_func([$model, 'findSlug'], $slug);
 
-    return view('notacms::' . $blog . '.show', compact('post'));
+    return view('notacms::'.$blog.'.show', compact('post'));
 })->whereIn('blog', array_keys(config('notacms')));
